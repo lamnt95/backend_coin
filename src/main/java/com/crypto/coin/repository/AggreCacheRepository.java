f362448a35c44e65e39054eea88317b0bdd32f01
lamnt95 <34705561+lamnt95@users.noreply.github.com>
2022-04-08 16:44:24 +0700

    Update AggreCacheRepository.java

--- src/main/java/com/crypto/coin/repository/AggreCacheRepository.java
+++ src/main/java/com/crypto/coin/repository/AggreCacheRepository.java
@@ -3,6 +3,7 @@ package com.crypto.coin.repository;
 import com.crypto.coin.model.AggreCache;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface AggreCacheRepository extends JpaRepository<AggreCache, Long> {
