e88c4c836ee4d2cf24cbec5726892bd3de8ec680
lamnt95 <34705561+lamnt95@users.noreply.github.com>
2022-04-08 16:59:32 +0700

    Update AggreCache.java

--- src/main/java/com/crypto/coin/model/AggreCache.java
+++ src/main/java/com/crypto/coin/model/AggreCache.java
@@ -10,7 +10,7 @@ public class AggreCache {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(name = "data")
     private String data;
