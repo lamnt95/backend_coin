4f6692ba89f370ef98f83ca2271db98d882f67c7
lamnt95 <34705561+lamnt95@users.noreply.github.com>
2022-04-12 13:38:05 +0700

    Update Post2.java

--- src/main/java/com/crypto/coin/model/Post2.java
+++ src/main/java/com/crypto/coin/model/Post2.java
@@ -16,11 +16,20 @@ public class Post2 implements Serializable {
     private String n;
     private String a;
     private String d;
-
+    private String s;
+    
      public Post2(Integer id, String name, String articleType, String date) {
         this.i = id;
         this.n = name;
         this.d = date;
         this.a = articleType;
+    }
+    
+    public Post2(Integer id, String name, String articleType, String date, String s) {
+        this.i = id;
+        this.n = name;
+        this.d = date;
+        this.a = articleType;
+        this.s = s;
     }
 }
