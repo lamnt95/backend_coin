bf8d35da81aac9e9dab708cd01452febb7db1afa
lamnt95 <34705561+lamnt95@users.noreply.github.com>
2022-04-12 07:54:57 +0700

    Update PostRepo.java

--- src/main/java/com/crypto/coin/repository/PostRepo.java
+++ src/main/java/com/crypto/coin/repository/PostRepo.java
@@ -19,5 +19,8 @@ public interface PostRepo extends JpaRepository<Post, Long> {
     Post getOne(Integer id);
 
     @Query("select new com.crypto.coin.model.Post(a.id, a.name, a.source, a.articleType, a.slug, a.srcId, a.link, a.date) from Post a ")
+    List<Post> getAll3();
+    
+     @Query("select new com.crypto.coin.model.Post(a.id, a.name, a.articleType, a.link, a.date) from Post a ")
     List<Post> getAll2();
 }
