01a03e85764096d4c42373aa1ca9b69f436e3c75
lamnt95 <34705561+lamnt95@users.noreply.github.com>
2022-04-12 08:53:30 +0700

    Update CachingConfig.java

--- src/main/java/com/crypto/coin/service/CachingConfig.java
+++ src/main/java/com/crypto/coin/service/CachingConfig.java
@@ -1,9 +1,22 @@
+package com.crypto.coin.service;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+
 @Configuration
 @EnableCaching
 public class CachingConfig {
 
     @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("coin");
-    }
+  public CacheManager cacheManager() {
+      SimpleCacheManager cacheManager = new SimpleCacheManager();
+      cacheManager.setCaches(Arrays.asList(
+              new ConcurrentMapCache("coincache")
+	  ));
+      return cacheManager;
+  }
 }
