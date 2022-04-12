e783fb4a2ef5a1ed1562fe7ad8e0baa37ae5724d
lamnt95 <34705561+lamnt95@users.noreply.github.com>
2022-04-12 08:10:15 +0700

    Create Post2.java

---
+++ src/main/java/com/crypto/coin/model/Post2.java
@@ -0,0 +1,26 @@
+package com.crypto.coin.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post2 implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+    private String articleType;
+    private String date;
+
+     public Post2(Integer id, String name, String articleType, String date) {
+        this.id = id;
+        this.name = name;
+        this.articleType = articleType;
+        this.date = date;
+    }
+}
